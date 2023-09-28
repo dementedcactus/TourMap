0dc33a97c05cd2c903f37e844a0a4e1ff4825c92
Richard Crichlow <dementedcactus@gmail.com>
2023-09-27 20:56:55 -0500

    Initial Commit

---
+++ TourMap/TourMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourMapApp.swift
+//  TourMap
+//
+//  Created by Richard Crichlow on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TourMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
