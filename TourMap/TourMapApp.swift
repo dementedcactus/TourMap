04ea4035561c8136bb00c049c229fb80ad3fdf2e
Richard Crichlow <dementedcactus@gmail.com>
2023-09-27 21:00:54 -0500

    First Commit - iOS 17 - Xcode 15

--- TourMap/TourMapApp.swift
+++ TourMap/TourMapApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct TourMapApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            ToursView()
         }
     }
 }
---
+++ TourMap/ToursView.swift
@@ -0,0 +1,18 @@
+//
+//  ToursView.swift
+//  TourMap
+//
+//  Created by Richard Crichlow on 9/27/23.
+//
+
+import SwiftUI
+
+struct ToursView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToursView()
+}
