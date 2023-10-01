83607cbbff4fec35fd86612f13cbcb1ad825f662
Richard Crichlow <dementedcactus@gmail.com>
2023-09-30 22:09:20 -0500

    Added Sample Tour stops

---
+++ TourMap/Tour.swift
@@ -0,0 +1,90 @@
+//
+//  Tour.swift
+//  TourMap
+//
+//  Created by Richard Crichlow on 9/30/23.
+//
+
+import Foundation
+import MapKit
+
+struct Tour {
+    // Sample Tour Data
+    var phase1Stops: [CLLocationCoordinate2D] = [
+        // The 30th & Ames Commercial Center
+        CLLocationCoordinate2D(latitude: 41.2958, longitude: -95.9384),
+        // The Union For Contemporary Art
+        CLLocationCoordinate2D(latitude: 41.2840, longitude: -95.9374),
+        // The Bryant Center
+        CLLocationCoordinate2D(latitude: 41.2847, longitude: -95.9570),
+        // Fair Deal Market Place
+        CLLocationCoordinate2D(latitude: 41.2691, longitude: -95.9328),
+        // The Carver Bank
+        CLLocationCoordinate2D(latitude: 41.2642, longitude: -95.9322)
+    ]
+    
+    var phase2Stops: [CLLocationCoordinate2D] = [
+        // The 30th & Ames Commercial Center
+        CLLocationCoordinate2D(latitude: 41.2958, longitude: -95.9384),
+        // The Union For Contemporary Art
+        CLLocationCoordinate2D(latitude: 41.2840, longitude: -95.9374),
+        // The Bryant Center
+        CLLocationCoordinate2D(latitude: 41.2847, longitude: -95.9570),
+        // Fair Deal Market Place
+        CLLocationCoordinate2D(latitude: 41.2691, longitude: -95.9328),
+        // The Carver Bank
+        CLLocationCoordinate2D(latitude: 41.2642, longitude: -95.9322)
+    ]
+    
+    var phase3Stops: [CLLocationCoordinate2D] = [
+        // The 30th & Ames Commercial Center
+        CLLocationCoordinate2D(latitude: 41.2958, longitude: -95.9384),
+        // The Union For Contemporary Art
+        CLLocationCoordinate2D(latitude: 41.2840, longitude: -95.9374),
+        // The Bryant Center
+        CLLocationCoordinate2D(latitude: 41.2847, longitude: -95.9570),
+        // Fair Deal Market Place
+        CLLocationCoordinate2D(latitude: 41.2691, longitude: -95.9328),
+        // The Carver Bank
+        CLLocationCoordinate2D(latitude: 41.2642, longitude: -95.9322)
+    ]
+    
+    var northOmahaTourStops: [CLLocationCoordinate2D] = [
+        // Salem Baptist Church
+        CLLocationCoordinate2D(latitude: 41.2931, longitude: -95.9275),
+        // The Great Plains Black History Museum
+        CLLocationCoordinate2D(latitude: 41.2853, longitude: -95.9381),
+        // The Love’s Jazz & Arts Center
+        CLLocationCoordinate2D(latitude: 41.2822, longitude: -95.9380),
+        // The Eagle's Nest Worship Center
+        CLLocationCoordinate2D(latitude: 41.2992, longitude: -95.9592),
+        // Omaha North High School
+        CLLocationCoordinate2D(latitude: 41.2986, longitude: -95.9384)
+    ]
+    
+    let historicalTour: [CLLocationCoordinate2D] = [
+        // The 30th & Ames Commercial Center
+        CLLocationCoordinate2D(latitude: 41.2958, longitude: -95.9384),
+        // The Union For Contemporary Art
+        CLLocationCoordinate2D(latitude: 41.2840, longitude: -95.9374),
+        // The Bryant Center
+        CLLocationCoordinate2D(latitude: 41.2847, longitude: -95.9570),
+        // Fair Deal Market Place
+        CLLocationCoordinate2D(latitude: 41.2691, longitude: -95.9328),
+        // The Carver Bank
+        CLLocationCoordinate2D(latitude: 41.2642, longitude: -95.9322)
+    ]
+    
+    var sampleStops: [CLLocationCoordinate2D] = [
+        // The 30th & Ames Commercial Center
+        CLLocationCoordinate2D(latitude: 41.2958, longitude: -95.9384),
+        // The Union For Contemporary Art
+        CLLocationCoordinate2D(latitude: 41.2840, longitude: -95.9374),
+        // The Bryant Center
+        CLLocationCoordinate2D(latitude: 41.2847, longitude: -95.9570),
+        // Fair Deal Market Place
+        CLLocationCoordinate2D(latitude: 41.2691, longitude: -95.9328),
+        // The Carver Bank
+        CLLocationCoordinate2D(latitude: 41.2642, longitude: -95.9322)
+    ]
+}
