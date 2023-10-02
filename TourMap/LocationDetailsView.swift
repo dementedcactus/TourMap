dc9562547027e79cd331ef9719b264f836da5932
Richard Crichlow <dementedcactus@gmail.com>
2023-10-02 12:56:51 -0500

    Base setup for MapKit refactor

---
+++ TourMap/LocationDetailsView.swift
@@ -0,0 +1,48 @@
+//
+//  LocationDetailsView.swift
+//  TourMap
+//
+//  Created by Richard Crichlow on 10/2/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationDetailsView: View {
+    @Binding var mapSelection: MKMapItem?
+    @Binding var show: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(mapSelection?.placemark.name ?? "")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    Text(mapSelection?.placemark.title ?? "")
+                        .font(.footnote)
+                        .foregroundStyle(.gray)
+                        .lineLimit(2)
+                        .padding(.trailing)
+                }
+                
+                Spacer()
+                
+                Button {
+                    show.toggle()
+                    mapSelection = nil
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(.gray, Color(.systemGray6))
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LocationDetailsView(mapSelection: .constant(nil), show: .constant(false))
+}
